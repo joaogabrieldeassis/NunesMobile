2bac7cd3e12429e826091094f8d61025485896c7
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2024-02-14 20:29:52 -0300

    Criando os modelos

--- appcaixa/lib/main.dart
+++ appcaixa/lib/main.dart
@@ -1,344 +1,49 @@
+import 'package:appcaixa/Models/Produto.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+main() => runApp(ExpensesApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  final List<Produto> produtosInializados = Produto.iniciarProdutos();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return // Generated code for this Stack Widget...
-        Stack(
-      children: [
-        Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [],
-        ),
-        Align(
-          alignment: AlignmentDirectional(-0.03, -0.51),
-          child: Text(
-            'Numero do pedido\n',
-            style: FlutterFlowTheme.of(context).bodyMedium.override(
-                  fontFamily: 'Readex Pro',
-                  fontSize: 17,
-                ),
-          ),
-        ),
-        Align(
-          alignment: AlignmentDirectional(-0.03, -0.11),
-          child: Text(
-            'Quantidade',
-            style: FlutterFlowTheme.of(context).bodyMedium.override(
-                  fontFamily: 'Readex Pro',
-                  fontSize: 17,
-                ),
-          ),
-        ),
-        Align(
-          alignment: AlignmentDirectional(-0.49, 0.46),
-          child: FFButtonWidget(
-            onPressed: () {
-              print('Button pressed ...');
-            },
-            text: 'Limpar',
-            options: FFButtonOptions(
-              height: 40,
-              padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
-              iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-              color: FlutterFlowTheme.of(context).tertiary,
-              textStyle: FlutterFlowTheme.of(context).titleSmall.override(
-                    fontFamily: 'Readex Pro',
-                    color: Colors.white,
-                  ),
-              elevation: 3,
-              borderSide: BorderSide(
-                color: Colors.transparent,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(8),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nunes-Caixa'),
+      ),
+      body: Row(children: <Widget>[
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            //<-- VEJA AQUI
+            side: BorderSide(
+              width: 1.0,
             ),
           ),
+          child: Text('Adicionar'),
         ),
-        Align(
-          alignment: AlignmentDirectional(0.6, 0.46),
-          child: FFButtonWidget(
-            onPressed: () {
-              print('Button pressed ...');
-            },
-            text: 'Adicionar',
-            options: FFButtonOptions(
-              height: 40,
-              padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
-              iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-              color: FlutterFlowTheme.of(context).success,
-              textStyle: FlutterFlowTheme.of(context).titleSmall.override(
-                    fontFamily: 'Readex Pro',
-                    color: Colors.white,
-                  ),
-              elevation: 3,
-              borderSide: BorderSide(
-                color: Colors.transparent,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(8),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            //<-- VEJA AQUI
+            side: BorderSide(
+              width: 1.0,
             ),
           ),
+          child: Text('Limpar'),
         ),
-        Align(
-          alignment: AlignmentDirectional(0, -0.38),
-          child: Padding(
-            padding: EdgeInsets.all(24),
-            child: TextFormField(
-              controller: _model.textController1,
-              focusNode: _model.textFieldFocusNode1,
-              autofocus: true,
-              obscureText: false,
-              decoration: InputDecoration(
-                labelText: '0',
-                labelStyle: FlutterFlowTheme.of(context).labelMedium.override(
-                      fontFamily: 'Readex Pro',
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                    ),
-                alignLabelWithHint: false,
-                hintStyle: FlutterFlowTheme.of(context).labelSmall,
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0x00000000),
-                    width: 29,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                    topLeft: Radius.circular(22),
-                    topRight: Radius.circular(22),
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: FlutterFlowTheme.of(context).primary,
-                    width: 29,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                    topLeft: Radius.circular(22),
-                    topRight: Radius.circular(22),
-                  ),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: FlutterFlowTheme.of(context).error,
-                    width: 29,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                    topLeft: Radius.circular(22),
-                    topRight: Radius.circular(22),
-                  ),
-                ),
-                focusedErrorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: FlutterFlowTheme.of(context).error,
-                    width: 29,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                    topLeft: Radius.circular(22),
-                    topRight: Radius.circular(22),
-                  ),
-                ),
-                filled: true,
-                fillColor: FlutterFlowTheme.of(context).alternate,
-              ),
-              style: FlutterFlowTheme.of(context).bodyMedium.override(
-                    fontFamily: 'Readex Pro',
-                    fontWeight: FontWeight.w500,
-                  ),
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.number,
-              validator: _model.textController1Validator.asValidator(context),
-            ),
-          ),
-        ),
-        Align(
-          alignment: AlignmentDirectional(0, 0.1),
-          child: Padding(
-            padding: EdgeInsets.all(24),
-            child: TextFormField(
-              controller: _model.textController2,
-              focusNode: _model.textFieldFocusNode2,
-              autofocus: true,
-              obscureText: false,
-              decoration: InputDecoration(
-                labelText: '0',
-                labelStyle: FlutterFlowTheme.of(context).labelMedium.override(
-                      fontFamily: 'Readex Pro',
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.normal,
-                    ),
-                alignLabelWithHint: false,
-                hintStyle: FlutterFlowTheme.of(context).labelSmall,
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color(0x00000000),
-                    width: 29,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                    topLeft: Radius.circular(22),
-                    topRight: Radius.circular(22),
-                  ),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: FlutterFlowTheme.of(context).primary,
-                    width: 29,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                    topLeft: Radius.circular(22),
-                    topRight: Radius.circular(22),
-                  ),
-                ),
-                errorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: FlutterFlowTheme.of(context).error,
-                    width: 29,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                    topLeft: Radius.circular(22),
-                    topRight: Radius.circular(22),
-                  ),
-                ),
-                focusedErrorBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: FlutterFlowTheme.of(context).error,
-                    width: 29,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(22),
-                    bottomRight: Radius.circular(22),
-                    topLeft: Radius.circular(22),
-                    topRight: Radius.circular(22),
-                  ),
-                ),
-                filled: true,
-                fillColor: FlutterFlowTheme.of(context).alternate,
-              ),
-              style: FlutterFlowTheme.of(context).bodyMedium.override(
-                    fontFamily: 'Readex Pro',
-                    fontWeight: FontWeight.w500,
-                  ),
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.number,
-              validator: _model.textController2Validator.asValidator(context),
-            ),
-          ),
-        ),
-        Align(
-          alignment: AlignmentDirectional(0.69, -0.85),
-          child: Text(
-            '0',
-            textAlign: TextAlign.center,
-            style: FlutterFlowTheme.of(context).bodyMedium.override(
-                  fontFamily: 'Readex Pro',
-                  fontSize: 16,
-                ),
-          ),
-        ),
-        Opacity(
-          opacity: 10,
-          child: Align(
-            alignment: AlignmentDirectional(1.04, -1.02),
-            child: Padding(
-              padding: EdgeInsets.all(24),
-              child: Icon(
-                FFIcons.kaddShoppingCart,
-                color: FlutterFlowTheme.of(context).secondaryText,
-                size: 35,
-              ),
-            ),
-          ),
-        ),
-      ],
+        
+      ]),
     );
   }
 }
